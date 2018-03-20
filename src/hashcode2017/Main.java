00621135bd297c71449a936253c1b9769d88ee9e
Alex <Alexander.Stephen.Wall@gmail.com>
2018-03-20 22:53:42 +0000

    Initial commit

---
+++ src/hashcode2017/Main.java
@@ -0,0 +1,237 @@
+package hashcode2017;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.Supplier;
+
+public class Main
+{
+	/* Possible filenames are 'small', 'med1', 'med2', and 'big'. */
+	private static final String	INPUT_FILENAME	= "input/big";
+	private static final String	OUTPUT_FILENAME	= "output/big";
+
+	static int		V, E, R, C, X;	/* Number of Videos, Endpoints, Requests, Cache Servers, and Capacity of Cache Servers. */
+	static int[]	vidSizes;		/* Size of each video (in MB). */
+	static int[][]	latencies;		/* Array of latencies between a cache server and the endpoint. */
+	static int[][]	requests;		/* Array of requests of endpoint for each video. */
+
+	public static void main ( String[] args )
+	{
+		getInput();
+		ArrayList<Integer>[] videosToStoreInCache = assignVideosToCaches();
+		output( videosToStoreInCache );
+	}
+
+	/** Get input from INPUT_FILENAME. */
+	private static void getInput ()
+	{
+		int numConnections;
+		int[] intArray; /* Used to get and assign input. */
+
+		try ( BufferedReader br = new BufferedReader( new FileReader( new File( INPUT_FILENAME ) ) ) )
+		{
+			/* Create lambda function to get input line as array of ints. */
+			Supplier<int[]> nextLineAsIntArray = () -> {
+				try
+				{
+					return Arrays.asList( br.readLine().trim().split( "\\s+" ) ).stream().mapToInt( Integer::parseInt ).toArray();
+				}
+				catch ( IOException e )
+				{
+					e.printStackTrace();
+				}
+				return null;
+			};
+
+			intArray = nextLineAsIntArray.get();
+
+			V = intArray[ 0 ]; /* Number of Videos. */
+			E = intArray[ 1 ]; /* Number of Endpoints. */
+			R = intArray[ 2 ]; /* Number of Request Descriptions. */
+			C = intArray[ 3 ]; /* Number of Cache Servers. */
+			X = intArray[ 4 ]; /* Capacity of the Cache Servers (in MB). */
+
+			vidSizes = nextLineAsIntArray.get(); /* Get the size of each video (in MB). */
+
+			latencies = new int[ C + 1 ][ E ]; /* Array of latencies between a cache server and the endpoint. */
+			for ( int[] row : latencies )
+			{
+				Arrays.fill( row, Integer.MAX_VALUE ); /* Initially there is no connection. */
+			}
+
+			for ( int i = 0; i < E; ++i ) /* For each endpoint... */
+			{
+				intArray = nextLineAsIntArray.get(); /* Find out the latency to the MainFrame and how many connections to Cache servers there are. */
+
+				latencies[ C ][ i ] = intArray[ 0 ]; /* Latency of endpoint to MainFrame. */
+				numConnections = intArray[ 1 ]; /* How many Cache servers there are connected to the endpoint. */
+
+				for ( int j = 0; j < numConnections; ++j ) /* For each connection to a Cache server... */
+				{
+					intArray = nextLineAsIntArray.get();
+
+					latencies[ intArray[ 0 ] ][ i ] = intArray[ 1 ]; /* Latency from this Cache server to the endpoint is set. */
+				}
+			}
+
+			requests = new int[ E ][ V ]; /* Each endpoint has requests for each video as zero initially. */
+
+			for ( int i = 0; i < R; ++i ) /* For each request... */
+			{
+				intArray = nextLineAsIntArray.get();
+				requests[ intArray[ 1 ] ][ intArray[ 0 ] ] += intArray[ 2 ]; /* The requests for vidId at endptId increased. */
+			}
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+			return;
+		}
+	}
+
+	/** Assign the videos to the caches. */
+	private static ArrayList<Integer>[] assignVideosToCaches ()
+	{
+		int[][] currentQuickestLatencies = new int[ E ][ V ]; /* The lowest latency with which an endpoint can get a video. This is dependent on the caches and therefore changes. */
+
+		/* Initially the quickest way for an endpoint to get a video is from the MainFrame. */
+		for ( int i = 0; i < E; i++ )
+		{
+			for ( int j = 0; j < V; j++ )
+			{
+				currentQuickestLatencies[ i ][ j ] = latencies[ C ][ i ];
+			}
+		}
+
+		int[] knapsack = new int[ X + 1 ]; /* Knapsack array to place video in cache. */
+		int[] kValues = new int[ V ]; /* The value of each video for the cache. */
+
+		@SuppressWarnings ( "unchecked" )
+		ArrayList<Integer>[] videosToStoreInCache = new ArrayList[ C ];
+
+		System.out.println( "Completition:\n0.0%" );
+		for ( int i = 0; i < C; i++ ) /* For each cache... */
+		{
+			Arrays.fill( knapsack, 0 );
+
+			videosToStoreInCache[ i ] = new ArrayList<Integer>();
+
+			calculateKnapsackValues( kValues, currentQuickestLatencies, i );
+			solveKnapsack( knapsack, kValues );
+			videosToStoreInCache[ i ] = recoverKnapsackSolution( knapsack, kValues );
+
+			/* Update quickest way to access a video for an endpoint. */
+			for ( int j = 0; j < videosToStoreInCache[ i ].size(); j++ )
+			{
+				for ( int k = 0; k < E; k++ )
+				{
+					currentQuickestLatencies[ k ][ videosToStoreInCache[ i ].get( j ) ] = Math.min( currentQuickestLatencies[ k ][ videosToStoreInCache[ i ].get( j ) ], latencies[ i ][ k ] );
+				}
+			}
+
+			/* Print progress if a multiple of 0.1%. */
+			if ( ( i + 1 ) * 1000 % C == 0 )
+			{
+				System.out.println( ( i + 1 ) * 100.0 / C + "%" );
+			}
+		}
+		return videosToStoreInCache;
+	}
+
+	/** Work out the knapsack values for each video with respect to this cache. */
+	private static void calculateKnapsackValues ( int[] kValues, int[][] quickLatencies, int cacheNumber )
+	{
+		for ( int j = 0; j < V; j++ ) /* For each video... */
+		{
+			kValues[ j ] = 0;
+			for ( int k = 0; k < E; k++ ) /* For each endpoint... */
+			{
+				/* Add to the value of video j a score -- the difference between the fastest latency and the MainFrame, multipled by the number of requests. */
+				kValues[ j ] += ( quickLatencies[ k ][ j ] - Math.min( latencies[ cacheNumber ][ k ], quickLatencies[ k ][ j ] ) ) * requests[ k ][ j ];
+			}
+		}
+	}
+
+	/** Solve the knapsack problem. */
+	private static void solveKnapsack ( int[] knapsack, int[] kValues )
+	{
+		if ( kValues[ 0 ] <= X )
+		{
+			knapsack[ vidSizes[ 0 ] ] = X;
+		}
+
+		for ( int a = 1; a < V; a++ )
+		{
+			for ( int b = X; b >= 0; b-- )
+			{
+				if ( b - vidSizes[ a ] >= 0 )
+				{
+					knapsack[ b ] = Math.max( knapsack[ b ], knapsack[ b - vidSizes[ a ] ] + kValues[ a ] );
+				}
+			}
+		}
+	}
+
+	/** Create list of videos to add based on knapsack solution. */
+	private static ArrayList<Integer> recoverKnapsackSolution ( int[] knapsack, int[] kValues )
+	{
+		@SuppressWarnings ( "unchecked" )
+		ArrayList<Integer>[] knapsackSolution = new ArrayList[ X + 1 ]; /* First entry is always the weight. */
+
+		for ( int y = X; y >= 0; y-- )
+		{
+			knapsackSolution[ y ] = new ArrayList<Integer>();
+			knapsackSolution[ y ].add( Integer.MAX_VALUE );
+		}
+
+		knapsackSolution[ X ].set( 0, knapsack[ X ] );
+
+		for ( int x = V - 1; x >= 0; x-- )
+		{
+			for ( int y = vidSizes[ x ]; y <= X; y++ )
+			{
+				if ( knapsackSolution[ y ].get( 0 ) - kValues[ x ] < knapsackSolution[ y - vidSizes[ x ] ].get( 0 ) )
+				{
+					knapsackSolution[ y - vidSizes[ x ] ].clear();
+
+					knapsackSolution[ y - vidSizes[ x ] ].add( knapsackSolution[ y ].get( 0 ) - kValues[ x ] );
+					for ( int z = 1; z < knapsackSolution[ y ].size(); z++ )
+					{
+						knapsackSolution[ y - vidSizes[ x ] ].add( knapsackSolution[ y ].get( z ) );
+					}
+					knapsackSolution[ y - vidSizes[ x ] ].add( x );
+				}
+			}
+		}
+
+		knapsackSolution[ 0 ].remove( 0 );
+
+		return knapsackSolution[ 0 ];
+	}
+
+	/** Output assignment of videos to caches found. */
+	private static void output ( ArrayList<Integer>[] videosStoredInCaches )
+	{
+		try ( BufferedWriter bw = new BufferedWriter( new FileWriter( OUTPUT_FILENAME ) ) )
+		{
+			bw.write( "" + videosStoredInCaches.length );
+			for ( int i = 0; i < videosStoredInCaches.length; i++ )
+			{
+				bw.write( "\n" + i );
+				for ( int j = videosStoredInCaches[ i ].size() - 1; j >= 0; j-- )
+				{
+					bw.write( " " + videosStoredInCaches[ i ].get( j ) );
+				}
+			}
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/hashcode2018/HashCode2018.java
@@ -0,0 +1,337 @@
+package hashcode2018;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+
+/**
+ * @author Alex
+ */
+public class HashCode2018
+{
+	// === Hyperparameters ===
+
+	/* File to deal with —— { A = a_example, B = b_should_be_easy, C = c_no_hurry, D = d_metropolis, E = e_high_bonus }. */
+	private static final Files file = Files.D;
+
+	/* The power of the numerator in the score function. */
+	private static final int POW = 1;
+
+	/* The score function that calculates the score of a ride for a car — used when greedily trying to assign rides to cars. */
+	private static final BiFunction<CarInfo, RideInfo, Double> scoreFunc = ( car, ride ) -> {
+		return ( Math.pow( ride.dist + getBonusModifier( car, ride ), POW ) ) / ( getTotalCarJourneyTime( car, ride ) );
+	};
+
+	// === The problem instance variables ===
+
+	static int	R;	/* Number of rows of the grid. */
+	static int	C;	/* Number of columns of the grid. */
+	static int	F;	/* Number of vehicles in the fleet. */
+	static int	N;	/* Number of rides. */
+	static int	B;	/* Per-ride bonus for starting the ride on time. */
+	static int	T;	/* Number of steps in the simulation. */
+
+	static List<RideInfo> rides;
+
+	static CarInfo[] cars;
+
+	public static void main ( String[] args )
+	{
+		getInput( file );
+		List<List<Integer>> solution = getAssignment( scoreFunc );
+		output( file, solution );
+	}
+
+	/**
+	 * Gets the assignment of cars, indexed by ints, to the list of rides it will give, also index by ints.
+	 *
+	 * @param scoreFunc The function that calculates the score of a ride for a car — used when greedily trying to assign rides to cars.
+	 * @return A list which has, indexed by the integer value of the corresponding car, the rides ids the car has been allocated.
+	 */
+	private static List<List<Integer>> getAssignment ( BiFunction<CarInfo, RideInfo, Double> scoreFunc )
+	{
+		List<List<Integer>> solution = new ArrayList<List<Integer>>();
+
+		for ( int i = 0; i < F; i++ )
+		{
+			solution.add( new ArrayList<Integer>() );
+		}
+
+		CarInfo car;
+		RideInfo ride;
+		double maxScore, score;
+		int maxRide;
+
+		outerloop: for ( int t = 0; t < T; t++ )
+		{
+			for ( int r = 0; r < rides.size(); r++ )
+			{
+				if ( rides.get( r ).f <= t )
+				{
+					rides.remove( rides.get( r ) );
+				}
+			}
+
+			for ( int c = 0; c < cars.length; c++ )
+			{
+				if ( rides.size() == 0 )
+				{
+					break outerloop;
+				}
+				car = cars[ c ];
+
+				/* Ensure car is available for this timestep. */
+				if ( car.t > t )
+				{
+					continue;
+				}
+				else if ( car.t < t )
+				{
+					car.t = t;
+				}
+
+				maxScore = -2;
+				maxRide = -2;
+
+				/* For available car, decide on a ride. */
+				for ( int r = 0; r < rides.size(); r++ )
+				{
+					ride = rides.get( r );
+					/* The ride may not be possible. */
+					if ( isJourneyInfeasible( car, ride ) )
+					{
+						continue;
+					}
+
+					score = scoreFunc.apply( car, ride );
+
+					if ( score > maxScore )
+					{
+						maxScore = score;
+						maxRide = r;
+					}
+				}
+
+				if ( maxRide == -2 )
+				{
+					continue;
+				}
+
+				ride = rides.get( maxRide );
+
+				solution.get( c ).add( ride.rideID );
+
+				car.t += getTotalCarJourneyTime( car, ride );
+				car.x = ride.x;
+				car.y = ride.y;
+
+				rides.remove( rides.get( maxRide ) );
+			}
+		}
+
+		return solution;
+	}
+
+	/** Get the potential bonus modifier if the car gives such a ride. */
+	private static double getBonusModifier ( CarInfo car, RideInfo ride )
+	{
+		return distToStart( car, ride ) <= ( car.t - ride.s ) ? B : 0;
+	}
+
+	/** Get how long it would take for the car to give the ride, including possibly waiting. */
+	static int getTotalCarJourneyTime ( CarInfo car, RideInfo ride )
+	{
+		return Math.max( distToStart( car, ride ), ride.s - car.t ) + ride.dist;
+	}
+
+	/** Gets whether the journey cannot be performed in time. */
+	private static boolean isJourneyInfeasible ( CarInfo car, RideInfo ride )
+	{
+		return car.t + getTotalCarJourneyTime( car, ride ) > ride.f;
+	}
+
+	/** Gets distance of car to start of ride. */
+	private static int distToStart ( CarInfo car, RideInfo ride )
+	{
+		return manhattenDist( car.x, car.y, ride.a, ride.b );
+	}
+
+	/** Gets Manhatten distance. */
+	private static int manhattenDist ( int x1, int y1, int x2, int y2 )
+	{
+		return Math.abs( x1 - x2 ) + Math.abs( y1 - y2 );
+	}
+
+	/** Get input from file. */
+	private static void getInput ( Files file )
+	{
+		int[] inputIntArray; /* Used to get and assign input. */
+
+		try ( BufferedReader br = new BufferedReader( new FileReader( new File( file.toInput() ) ) ) )
+		{
+			/* Create lambda function to get input line as array of ints. */
+			Supplier<int[]> nextLineAsIntArray = () -> {
+				try
+				{
+					return Arrays.asList( br.readLine().trim().split( "\\s+" ) ).stream().mapToInt( Integer::parseInt ).toArray();
+				}
+				catch ( IOException e )
+				{
+					e.printStackTrace();
+				}
+				return null;
+			};
+
+			inputIntArray = nextLineAsIntArray.get();
+
+			R = inputIntArray[ 0 ];
+			C = inputIntArray[ 1 ];
+			F = inputIntArray[ 2 ];
+			N = inputIntArray[ 3 ];
+			B = inputIntArray[ 4 ];
+			T = inputIntArray[ 5 ];
+
+			rides = new ArrayList<RideInfo>( N );
+
+			for ( int i = 0; i < N; i++ )
+			{
+				rides.add( new RideInfo( i, nextLineAsIntArray.get() ) );
+			}
+
+			/* Sort in order of soonest start of ride to latest. */
+			Collections.sort( rides, new Comparator<RideInfo>()
+			{
+				@Override
+				public int compare ( RideInfo r1, RideInfo r2 )
+				{
+					return r1.s - r2.s;
+				}
+			}.reversed() );
+
+			cars = new CarInfo[ F ];
+
+			for ( int i = 0; i < F; i++ )
+			{
+				cars[ i ] = new CarInfo();
+			}
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+			return;
+		}
+	}
+
+	/** Output solution to file with filename given. */
+	private static void output ( Files file, List<List<Integer>> solution )
+	{
+		try ( BufferedWriter bw = new BufferedWriter( new FileWriter( file.toOutput() ) ) )
+		{
+			for ( List<Integer> vehicleInfo : solution )
+			{
+				bw.write( Integer.toString( vehicleInfo.size() ) );
+				for ( int rideNum : vehicleInfo )
+				{
+					bw.write( " " + Integer.toString( rideNum ) );
+				}
+
+				if ( vehicleInfo == solution.get( solution.size() - 1 ) )
+				{
+					break;
+				}
+				bw.write( '\n' );
+			}
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private enum Files
+	{
+		A ( "a_example" ),
+		B ( "b_should_be_easy" ),
+		C ( "c_no_hurry" ),
+		D ( "d_metropolis" ),
+		E ( "e_high_bonus" );
+
+		public String str;
+
+		Files( String str )
+		{
+			this.str = str;
+		}
+
+		String toInput ()
+		{
+			return "input/" + str + ".in";
+		}
+
+		String toOutput ()
+		{
+			return "output/" + str + ".out";
+		}
+	}
+
+	private static class RideInfo
+	{
+		final int rideID;
+
+		int	a;	/* The row of the start intersection. */
+		int	b;	/* The column of the start intersection. */
+		int	x;	/* The row of the finish intersection. */
+		int	y;	/* The column of the finish intersection. */
+		int	s;	/* The earliest start time. */
+		int	f;	/* The latest finish time. (in [0,T], f >= s + |x-a| + |y-b|) */
+
+		int dist; /* Distance from start to end. */
+
+		public RideInfo( int rideID, int a, int b, int x, int y, int s, int f )
+		{
+			this.rideID = rideID;
+			this.a = a;
+			this.b = b;
+			this.x = x;
+			this.y = y;
+			this.s = s;
+			this.f = f;
+
+			dist = manhattenDist( a, b, x, y );
+		}
+
+		public RideInfo( int rideID, int[] in )
+		{
+			this( rideID, in[ 0 ], in[ 1 ], in[ 2 ], in[ 3 ], in[ 4 ], in[ 5 ] );
+		}
+	}
+
+	private static class CarInfo
+	{
+		int	x;	/* The row of the car. */
+		int	y;	/* The column of the car. */
+		int	t;	/* Time from which car is available. */
+
+		public CarInfo( int x, int y, int t )
+		{
+			this.x = x;
+			this.y = y;
+			this.t = t;
+		}
+
+		public CarInfo()
+		{
+			this( 0, 0, 0 );
+		}
+	}
+}
